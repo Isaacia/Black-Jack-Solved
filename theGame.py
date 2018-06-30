0c9a45623e748f2aec83d6052ba35c4220f2f0db
AllenovPeng <40724589+AllenovPeng@users.noreply.github.com>
2018-06-30 16:19:02 -0700

    Update theGame.py

--- theGame.py
+++ theGame.py
@@ -8,57 +8,117 @@ deck = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10,
 		"A", 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10,
 		"A", 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10,
 		"A", 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10,]
+
+pendingDeck = []
+
 usedDeck = []
 
-#Start Game
+playerWincount = 0
+
+supposeWin = 0
+
+def Hit()
+	checkRunout()
+	newCard = deck.pop(0)
+	pendingDeck += [newCard]
+	if newCard == "A":
+		countA += 1
+		Count += 11
+	else:
+		Count += newCard
+	return Count
+		
+	
+def RecursiveHit(sum)
+	if sum >= 21 and countA > 0
+		sum -= 10
+		countA -= 1
+	if sum >= 17:
+		return sum
+	else:
+		return RecursiveHit(sum.Hit())
+
+def checkRunout()
+	if deck == []:
+		deck = usedDeck
+		usedDeck = []
+		random.shuffle(deck) 
+
+#Start Game###################################
 
 random.shuffle(deck)    
 
+checkRunout()
 dealerC1 = deck.pop(0)	#visible to player
+checkRunout()
 dealerC2 = deck.pop(0)	#not visible
+checkRunout()
 playerC1 = deck.pop(0)
+checkRunout()
 playerC2 = deck.pop(0)
+pendingDeck += [dealerC1, dealerC2, playerC1, playerC2]
 
-#player's turn
+#player's turn#############################
 	import function
 	
 	
 	
-#dealer's turn
-dcountA = 0
+#dealer's turn##############################
+countA = 0
 dealerResult = 0
+Count = 0
 if dealerC1 == "A":
 	#ask player for insurance
 
-	dealerCount = 0
 if dealerC1 == "A":
 	dealerC1 = 11
-	dcountA += 1
+	countA += 1
 
 if dealerC2 == "A":
 	dealerC2 = 11
-	dcountA += 1
+	countA += 1
 	
-dealerCount = dealerC1 + dealerC2
+Count = dealerC1 + dealerC2
 
 #avoid two A
-if dealerCount > 21:
-	dealerCount -= 10
-	dcountA -= 1
+if Count > 21:
+	Count -= 10
+	countA -= 1
 
 
-if dealerCount <= 21 and dealerCount > 16:
-	dealerResult = dealerCount
+if Count <= 21 and Count > 16:
+	dealerResult = Count
 
-if dealerCount <= 16:
-	dealerCount.hit()
-	
-	
-	
+else: 
+	dealerResult = RecursiveHit(Count)
 
 	
 	
 	
-		else:
+#Compare
+if playerResult > 21:
+	print("Player Lose!")
+	playerWinCount -= supposeWin #depends on player's previous choice;
+
+elif dealerResult >21:
+	print ("Player Win!")
+	playerWinCount += supposeWin
+	
+else:
+	if playerResult > dealerResult:
 		print ("Player Win!")
-		playerWinCount += supposeWin #depends on player's previous choice;
+		playerWinCount += supposeWin
+	elif playerResult < dealerResult:
+		print("Player Lose!")
+		playerWinCount -= supposeWin
+	else:
+		print("Draw!")
+		
+usedDeck += pendingDeck
+pendingDeck = []
+		
+	
+	
+	
+
+	
