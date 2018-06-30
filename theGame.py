e63aff13472170b944c78d84da3a611a91ad808b
AllenovPeng <40724589+AllenovPeng@users.noreply.github.com>
2018-06-30 15:21:47 -0700

    Update theGame.py

--- theGame.py
+++ theGame.py
@@ -8,13 +8,57 @@ deck = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10,
 		"A", 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10,
 		"A", 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10,
 		"A", 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10,]
-newDeck = deck
+usedDeck = []
 
 #Start Game
 
 random.shuffle(deck)    
 
-dealerC1 = deck.pop(0)
-dealerC2 = deck.pop(0)
+dealerC1 = deck.pop(0)	#visible to player
+dealerC2 = deck.pop(0)	#not visible
 playerC1 = deck.pop(0)
 playerC2 = deck.pop(0)
+
+#player's turn
+	import function
+	
+	
+	
+#dealer's turn
+dcountA = 0
+dealerResult = 0
+if dealerC1 == "A":
+	#ask player for insurance
+
+	dealerCount = 0
+if dealerC1 == "A":
+	dealerC1 = 11
+	dcountA += 1
+
+if dealerC2 == "A":
+	dealerC2 = 11
+	dcountA += 1
+	
+dealerCount = dealerC1 + dealerC2
+
+#avoid two A
+if dealerCount > 21:
+	dealerCount -= 10
+	dcountA -= 1
+
+
+if dealerCount <= 21 and dealerCount > 16:
+	dealerResult = dealerCount
+
+if dealerCount <= 16:
+	dealerCount.hit()
+	
+	
+	
+
+	
+	
+	
+		else:
+		print ("Player Win!")
+		playerWinCount += supposeWin #depends on player's previous choice;
